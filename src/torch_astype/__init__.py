7d5941b2402af8db718d1ee474784f31e299a91b
Mathew Huerta-Enochian <mathewhe@gmail.com>
2024-01-27 15:12:34 +0900

    Add 0.0.1 version to __init__

--- src/torch_astype/__init__.py
+++ src/torch_astype/__init__.py
@@ -1,2 +1,5 @@
-from torch_astype.parse import parse_dtype, DTypeParseError
+__version__ = '0.0.1'
+
+
 import torch_astype.override_torch
+from torch_astype.parse import parse_dtype, DTypeParseError
